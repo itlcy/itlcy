a03be20877ce0f508d4dd31cfa83c7c3d8a82520
unknown <itlcy@sina.com>
2016-06-07 10:25:28 +0800

    添加项目

---
+++ 策划菜单/src/com/lcy/menu/view/slideMenu.java
@@ -0,0 +1,108 @@
+package com.lcy.menu.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.FrameLayout;
+
+public class slideMenu extends FrameLayout{
+
+	private View menuView,mainView;
+	private int menuWidth;
+	public slideMenu(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public slideMenu(Context context) {
+		super(context);
+		init();
+	}
+ 
+	private void init(){
+		
+		
+	}
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		menuView=getChildAt(0);
+		mainView=getChildAt(1);
+		 menuWidth = menuView.getLayoutParams().width;
+	}
+	/*@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		menuView.measure(menuWidth, heightMeasureSpec);
+		mainView.measure(widthMeasureSpec, heightMeasureSpec);
+		 
+	}*/
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		// TODO Auto-generated method stub
+		
+		switch (ev.getAction()) {
+         case MotionEvent.ACTION_UP:
+			downX=(int) ev.getX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int dx=(int)(ev.getX()-downX);
+			if(Math.abs(dx)>8)
+			{
+				return true;
+			}
+			break;
+			
+		
+		}
+		
+		return super.onInterceptTouchEvent(ev);
+		
+	}
+	@Override
+	protected void onLayout(boolean arg0, int l, int t, int r, int b) {
+		menuView.layout(-menuWidth, 0, 0, b);
+		mainView.layout(0, 0, r, b);
+		
+	}
+	private int downX;
+	private scrollAnimation animation; 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX=(int) event.getX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int moveX=(int) event.getX();
+			int dx=moveX-downX;
+			int scrollx=getScrollX()-dx;
+			if(scrollx<-menuWidth)scrollx=-menuWidth;
+			if(scrollx>0)scrollx=0;
+			scrollTo(scrollx, 0);
+			downX=moveX;
+			break;
+		case MotionEvent.ACTION_UP:
+			if(getScrollX()>-menuWidth/2)
+			{
+				animation = new scrollAnimation(0, this);
+			}else
+			{
+				animation = new scrollAnimation(-menuWidth, this);
+			}
+			startAnimation(animation);
+			break;
+
+		default:
+			break;
+		}
+		return true;
+	}
+
+
+	
+}
